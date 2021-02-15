eaa82e4a98cd6e7804393acc041ad9a08b78f5d3
Lord Rabbit <micaelsap357@gmail.com>
2021-02-15 19:00:01 +0200

    Done with user routes of backend

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express')
+var bodyParser = require('body-parser')
 const user = require("./user/routes")
+
 const app = express()
 const port = 3000
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
--- user/routes.js
+++ user/routes.js
@@ -1,19 +1,34 @@
 var express = require('express')
+const path = require("path")
 const fs = require("fs")
 
 var router = express.Router()
-let rawdata = fs.readFileSync('./users.json');
+let rawdata = fs.readFileSync(path.resolve(__dirname, "./users.json"));
 let users = JSON.parse(rawdata);
 
 
 router.get('/:userId', function (req, res) {
   var id = req.params["userId"]
-  res.send(users.find(user => user["userId"] == id))
+  user = users.find(user => user["userId"] == id)
+  delete user["password"]
+  delete user["roles"]
+  res.send(user)
 })
 
 
 router.post('/login', function (req, res) {
-  res.send('TBD')
+  var id = req.body.userId
+  var password = req.body.password
+  user = users.find(user => user["userId"] == id)
+  if(user["password"] == password)
+  {
+      delete user["password"]
+      res.send(user)
+  }
+  else
+  {
+      res.send('Wrong Password')
+  }
 })
 
 module.exports = router
